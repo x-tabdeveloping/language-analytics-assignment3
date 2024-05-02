009bed8e3d67d8bd5113810e31e9dd9dbb898015
Márton Kardos <power.up1163@gmail.com>
2024-05-02 11:00:32 +0200

    Added CO2 tracking and a nicer readme

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # language-analytics-assignment3
 Third Assignment for Language Analytics in Cultural Data Science
 
+The assignments is about using word embeddings for expanding search queries in a lyrics database.
+The code finds how many, and what proportion of a given artists' songs contain a given keyword or words semantically closely related to that keyword.
+
+I chose a fault tolerant approach, this has the following implications:
+ - If the given artist can't be exactly matched, you will get a warning, but the code will continue running with the closest fuzzy match. 
+ - If the given keyword can't be exactly matched in the embedding models' vocabulary, you will get a warning, but the code will continue running with the closest fuzzy match. 
+
 ## Setup
+
+You will need to download the [Spotfy Million Song dataset](https://www.kaggle.com/datasets/joebeachcapital/57651-spotify-songs).
+the file should be placed in a `dat` directory.
+
+```
+- dat/
+    - "Spotify Million Song Dataset_exported.csv"
+```
 
 Install dependencies:
 
@@ -11,13 +26,41 @@ pip install -r requirements.txt
 
 ## Usage
 
-You can query songs by using the `src/query.py` script.
+You can query songs by using the `src/query.py` command line interface for checking how many songs of a given artist contain a keyword or closesly related word.
 
 ```bash
 python3 src/query -a "Steely Dan" -w "cousin"
 ```
+```
+The most similar terms are the following: cousin, nephew, brother, son, uncle, eldest, grandson, daughter, father, grandfather, niece
+-------------------------------------------------------------
 
-## Interface
+Artist Steely Dan has 15 songs (17.05%) that contain words related to cousin.
+-------------------------------------------------------------
+```
+
+By passing the `--print_songs` flag you can also see the individual songs containing these terms.
+
+```
+These are: 
+ - Cousin Dupree
+ - Almost Gothic
+ - Babylon Sisters
+ - Chain Lightning
+ - Deacon Blues
+ - Don't Take Me Alive
+ - Godwhacker
+ - Green Flower Street
+ - Kid Charlemagne
+ - Pixeleen
+ - Pretzel Logic
+ - Sign In Stranger
+ - Time Out Of Mind
+ - Turn That Heartbeat Over Again
+ - Two Against Nature
+```
+
+### Parameters
 
 | Parameter | Description | Default |
 | - | - | - |
@@ -25,3 +68,6 @@ python3 src/query -a "Steely Dan" -w "cousin"
 | `-w` or `--query_word` | The seed word to base the semantic query on. | - |
 | `-k` or `--k_expansion` | Number of termas most similar to the seed term to include in the query. | `10` |
 | `--print_songs` | Flag to indicate whether the names of the songs should be printed. | `False` |
+
+> Additionally the script will produce csv files with the CO2 emissions of the substasks in the code (`emissions/`).
+> This is necessary for Assignment 5, and is not directly relevant to this assignment.
--- src/query.py
+++ src/query.py
@@ -1,9 +1,11 @@
 import argparse
 import warnings
+from pathlib import Path
 
 import gensim.downloader as api
 import numpy as np
 import pandas as pd
+from codecarbon import EmissionsTracker
 from gensim.utils import tokenize
 from thefuzz import process
 
@@ -43,34 +45,53 @@ def contains_search_term(text: str, search_terms: list[str]) -> bool:
 def main():
     parser = create_parser()
     args = parser.parse_args()
-    wv = api.load("glove-wiki-gigaword-50")
-    songs = pd.read_csv("dat/Spotify Million Song Dataset_exported.csv")
-    # Fuzzy finding artist
-    artist, _ = process.extractOne(args.artist, songs["artist"].unique())
-    if artist.lower() != args.artist.lower():
-        warnings.warn(f"{args.artist} not found, the closest match is {artist}.")
-    # Filtering for artist
-    songs = songs[songs["artist"] == artist]
-    # Fuzzy finding query word
-    query_word, _ = process.extractOne(args.query_word, list(wv.index_to_key))
-    if query_word.lower() != args.query_word.lower():
-        warnings.warn(
-            f"{args.query_word} not found, the closest match is {query_word}."
+    emissions_dir = Path("emissions")
+    emissions_dir.mkdir(exist_ok=True)
+    with EmissionsTracker(
+        project_name="query_expansion",
+        save_to_file=True,
+        output_file="emissions.csv",
+        output_dir=emissions_dir,
+    ) as tracker:
+        tracker.start_task("load_model")
+        wv = api.load("glove-wiki-gigaword-50")
+        tracker.stop_task()
+        tracker.start_task("load_data")
+        songs = pd.read_csv("dat/Spotify Million Song Dataset_exported.csv")
+        tracker.stop_task()
+        tracker.start_task("fuzzy_finding")
+        # Fuzzy finding artist
+        artist, _ = process.extractOne(args.artist, songs["artist"].unique())
+        if artist.lower() != args.artist.lower():
+            warnings.warn(f"{args.artist} not found, the closest match is {artist}.")
+        # Filtering for artist
+        songs = songs[songs["artist"] == artist]
+        # Fuzzy finding query word
+        query_word, _ = process.extractOne(args.query_word, list(wv.index_to_key))
+        if query_word.lower() != args.query_word.lower():
+            warnings.warn(
+                f"{args.query_word} not found, the closest match is {query_word}."
+            )
+        tracker.stop_task()
+        tracker.start_task("query_expansion")
+        most_similar = wv.most_similar(positive=query_word, topn=args.k_expansion)
+        search_words = [query_word] + [word.lower() for word, _ in most_similar]
+        tracker.stop_task()
+        tracker.start_task("querying")
+        # Tokenizing songs
+        songs["contains_search_term"] = songs["text"].map(
+            lambda t: contains_search_term(t, search_words)
         )
-    most_similar = wv.most_similar(positive=query_word, topn=args.k_expansion)
-    search_words = [query_word] + [word.lower() for word, _ in most_similar]
+        n_total = len(songs.index)
+        n_songs_containing_search_terms = np.sum(songs["contains_search_term"])
+        percentage = (n_songs_containing_search_terms / n_total) * 100
+        tracker.stop_task()
+    print("-------------------------------------------------------------\n")
     print(
         "\nThe most similar terms are the following: {terms}".format(
             terms=", ".join(search_words)
         )
     )
-    # Tokenizing songs
-    songs["contains_search_term"] = songs["text"].map(
-        lambda t: contains_search_term(t, search_words)
-    )
-    n_total = len(songs.index)
-    n_songs_containing_search_terms = np.sum(songs["contains_search_term"])
-    percentage = (n_songs_containing_search_terms / n_total) * 100
     print("-------------------------------------------------------------\n")
     print(
         f"Artist {artist} has {n_songs_containing_search_terms} songs ({percentage:.2f}%) that contain words related to {query_word}."
